a77e802f39b299ffb09cc8dab68dd7f60702ce9b
RodrigoApaza <lightdestroyers.arca@gmail.com>
2023-03-14 19:34:32 -0400

    Agregando vista en celular

--- script.js
+++ script.js
@@ -4,12 +4,6 @@ function cambiaMensaje(){
     document.getElementById('loading_message').innerText = "Abrazo enviado";    
 }
 
-setInterval(function () {
-    document.getElementById('message').innerText = "Te quiero mucho Alina :3";
-}, 9800);
 
-setInterval(function () {
-    document.getElementById('message2').innerText = "No olvides brillar el dia hoy, chica guapa ;3";
-}, 9800);
 
 
