76307e1f14d8d087d14ffc4cebc6523b240eb4f1
RodrigoApaza <lightdestroyers.arca@gmail.com>
2023-03-14 19:00:06 -0400

    Version inicial del programa

---
+++ script.js
@@ -0,0 +1,15 @@
+setInterval(cambiaMensaje, 9500);
+
+function cambiaMensaje(){
+    document.getElementById('loading_message').innerText = "Abrazo enviado";    
+}
+
+setInterval(function () {
+    document.getElementById('message').innerText = "Te quiero mucho Alina :3";
+}, 9800);
+
+setInterval(function () {
+    document.getElementById('message2').innerText = "No olvides brillar el dia hoy, chica guapa ;3";
+}, 9800);
+
+
